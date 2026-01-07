371a8013cc1f5a7388def7d00cd8851534049b27
nawamondamon <maona5423@gmail.com>
2026-01-07 19:06:03 +0900

    Add files via upload

---
+++ js/aqjs.js
@@ -0,0 +1,54 @@
+let accordionDetails = '.js-details';
+let accordionSummary = '.js-details-summary';
+let accordionContent = '.js-details-content';
+let speed = 500;
+let timer;
+// 変数定義
+const CLASS = "-active";
+let flg = false;
+let accordionFlg = false;
+
+let hamburger = document.getElementById("js-hamburger");
+let focusTrap = document.getElementById("js-focus-trap");
+let menu = document.querySelector(".js-nav-area");
+let accordionTrigger = document.querySelectorAll(".js-sp-accordion-trigger");
+let accordion = document.querySelectorAll(".js-sp-accordion");
+
+
+// メニュー展開時に背景を固定
+const backgroundFix = (bool) => {
+  const scrollingElement = () => {
+    const browser = window.navigator.userAgent.toLowerCase();
+    if ("scrollingElement" in document) return document.scrollingElement;
+    return document.documentElement;
+  };
+
+  const scrollY = bool
+    ? scrollingElement().scrollTop
+    : parseInt(document.body.style.top || "0");
+
+  const fixedStyles = {
+    height: "100vh",
+    position: "fixed",
+    top: `${scrollY * -1}px`,
+    left: "0",
+    width: "100vw"
+  };
+
+  Object.keys(fixedStyles).forEach((key) => {
+    document.body.style[key] = bool ? fixedStyles[key] : "";
+  });
+
+  if (!bool) {
+    window.scrollTo(0, scrollY * -1);
+  }
+};
+
+
+
+$(function() {
+　$('.Toggle').click(function() {
+   $(this).toggleClass('active');
+  $('.menu').toggleClass('open');
+ });
+});
